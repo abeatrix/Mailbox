ca6a3b232da4ecfd38ef661182957b45a1809900
Beatrix Woo <beatrixwho@gmail.com>
2021-03-23 20:03:23 -0700

    add send-email function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -8,6 +8,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+
+  // send email
+  document.querySelector('#compose-form').onsubmit = () => {
+    const recipients = document.querySelector('#compose-recipients').value,
+    subject = document.querySelector('#compose-subject').value,
+    body = document.querySelector('#compose-body').value;
+
+    fetch('/emails', {method: 'POST', body: JSON.stringify({
+    recipients: recipients,
+    subject: subject,
+    body: body,
+    })}).then(res=>res.json());
+    localStorage.clear();
+    load_mailbox('sent');
+  };
+
 });
 
 function compose_email() {
@@ -15,6 +31,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#email-body').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -27,7 +44,18 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-body').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+  fetch(`/emails/${mailbox}`)
+  .then(res=>res.json())
+  .then(emails=>{
+    emails.forEach(display_email(email,mailbox))
+  })
+}
+
+function display_email(email,mailbox){
+
 }
