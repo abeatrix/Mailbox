5d7e8bac3f4d73c049eed974dba1402814f3476a
Beatrix Woo <beatrixwho@gmail.com>
2021-03-24 20:17:33 -0700

    send email succesfully

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -8,43 +8,52 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
-
-  // send email
-  document.querySelector('#compose-form').onsubmit = () => {
-    const recipients = document.querySelector('#compose-recipients').value,
-    subject = document.querySelector('#compose-subject').value,
-    body = document.querySelector('#compose-body').value;
-
-    fetch('/emails', {method: 'POST', body: JSON.stringify({
-    recipients: recipients,
-    subject: subject,
-    body: body,
-    })}).then(res=>res.json());
-    localStorage.clear();
-    load_mailbox('sent');
-  };
-
+  
 });
+
 
 function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-  document.querySelector('#email-body').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
+  document.querySelector('#compose-form').onsubmit = send_email;
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+    // send email
+  function send_email() {
+    console.log('sending')
+    const r = document.querySelector('#compose-recipients').value,
+    s = document.querySelector('#compose-subject').value,
+    b = document.querySelector('#compose-body').value;
+
+    fetch('/emails', {
+      method: 'POST', 
+      body: JSON.stringify({
+        recipients: r,
+        subject: s,
+        body: b,
+      })
+    })
+    .then(res=>res.json())
+    localStorage.clear();
+    load_mailbox('inbox');
+    return false;
+  };
+
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-  document.querySelector('#email-body').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -52,10 +61,16 @@ function load_mailbox(mailbox) {
   fetch(`/emails/${mailbox}`)
   .then(res=>res.json())
   .then(emails=>{
-    emails.forEach(display_email(email,mailbox))
+    console.log(emails)
+    emails.forEach(email=>display_email(email,mailbox))
   })
+
 }
 
 function display_email(email,mailbox){
-
+  console.log('displaying'+mailbox)
+  console.log(email)
+  const email_div = document.createElement('div');
+  const recipient_div = document.createElement('div');
+  const subject_div = document.createElement('div');
 }
